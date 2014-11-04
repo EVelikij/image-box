0b030b64d46c048818c1dbfe646b1210d4b0d847
EVelikij <evelikij@gmail.com>
2014-11-04 23:36:16 +0200

    added navigation for side bar menu

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,21 @@
 var app = angular.module('image-box', ['ui.router']);
+
+app.config(function($stateProvider) {
+  $stateProvider
+    .state('galleries', {
+      url: '/galleries',
+      templateUrl: 'partials/galleries.html'
+    })
+    .state('social', {
+      url: 'social',
+      templateUrl: 'partials/social.html'
+    })
+    .state('messages', {
+      url: 'messages',
+      templateUrl: 'partials/messages.html'
+    })
+    .state('settings', {
+      url: 'settings',
+      templateUrl: 'partials/settings.html'
+    })
+});
