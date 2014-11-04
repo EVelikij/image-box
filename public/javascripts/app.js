547abd763643fb2026b7f20e6e6ede67a04e236b
EVelikij <evelikij@gmail.com>
2014-11-05 00:14:09 +0200

    Current item in sidebar menu now is highlighted

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,15 +7,15 @@ app.config(function($stateProvider) {
       templateUrl: 'partials/galleries.html'
     })
     .state('social', {
-      url: 'social',
+      url: '/social',
       templateUrl: 'partials/social.html'
     })
     .state('messages', {
-      url: 'messages',
+      url: '/messages',
       templateUrl: 'partials/messages.html'
     })
     .state('settings', {
-      url: 'settings',
+      url: '/settings',
       templateUrl: 'partials/settings.html'
     })
 });
---
+++ public/javascripts/directives/sidebar-nav.js
@@ -0,0 +1,16 @@
+app.directive('boxSidebarNav', function($rootScope, $location) {
+  return {
+    restrict: 'A',
+    scope: {},
+    link: function(scope, el, attrs) {
+      $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        var href = el.attr('href');
+
+        if (toState.url == href)
+          el.addClass('active');
+        else
+          el.removeClass('active');
+      });
+    }
+  }
+});
