888db76179645c2500f75972460beef9f79893b1
EVelikij <evelikij@gmail.com>
2014-11-05 22:00:06 +0200

    all customs js files now loads on index.html automaticaly

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+function walk(path, callback) {
+  var getFiles = function(path) {
+    var paths = [];
+    fs.readdirSync(path).forEach(function(f) {
+      paths.push(path + '/' + f);
+    });
+
+    return paths;
+  }
+
+  // stack of files
+  var files = getFiles(path);
+
+  // loop on 
+  while (files.length)
+  {
+    var file = files.shift();
+    var stat = fs.statSync(file)
+
+    if (stat.isFile())
+      callback(file);
+    else
+      files = files.concat(getFiles(file));
+  }
+}
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  var publicPath = __dirname + '/../public';
+  var vendorsDir = '/javascripts/vendors/';
+  var jsFiles = [];
+
+
+  // get non vendors js files
+  walk(publicPath + '/javascripts', function(fullPath) {
+    // file name relative 'public' directory
+    var file = fullPath.slice(publicPath.length);
+
+    // save only javascript files
+    if (file.split('.').pop() == 'js')
+    {
+      if (file.indexOf(vendorsDir) != 0)
+        jsFiles.push(file);
+    }
+  });
+
+ console.log(jsFiles);
+  res.render('index', { js:jsFiles });
 });
 
 module.exports = router;
