50aa8db0fbcd79b6520380b6943dba91c1e842d8
EVelikij <evelikij@gmail.com>
2014-11-22 16:29:42 +0200

    added galleries controller to repository

---
+++ public/javascripts/controllers/galleriesCtrl.js
@@ -0,0 +1,20 @@
+module.exports = function($scope) {
+  $scope.data = {
+    facebook: [ { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
+                { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'}
+                ]
+  }
+}
