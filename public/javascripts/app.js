cec9be0930cdc7f65ad9428981a32f0d460f8e9c
EVelikij <evelikij@gmail.com>
2014-11-26 22:06:46 +0200

    Implemented functionality to handle access toke from vk.com and save it to cookies

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,8 @@
 var angular = require('angular');
 var uiRouter = require('angular-ui-router');
+var cookies = require('angular-cookies');
 
-var app = angular.module('image-box', ['ui.router']);
+var app = angular.module('image-box', ['ui.router', 'ngCookies']);
 app.config(function($stateProvider) {
   $stateProvider
     .state('galleries', {
@@ -20,6 +21,26 @@ app.config(function($stateProvider) {
       url: '/settings',
       templateUrl: 'partials/settings.html'
     })
+    .state('accessToken', {
+      url: '/access_token=:accessToken',
+      template: '',
+      controller: function ($location, $cookieStore) {
+        var hash = $location.path().substr(1);
+
+        var splitted = hash.split('&');
+        var params = {};
+
+        for (var i = 0; i < splitted.length; i++) {
+          var param  = splitted[i].split('=');
+          var key    = param[0];
+          var value  = param[1];
+          params[key] = value;
+          $cookieStore.put('accessToken', params);
+        }
+        $location.path("/about");
+      }
+    });
+  
 });
 
 app.directive('boxSidebarNav', require('./directives/sidebar-nav'));
--- public/javascripts/controllers/galleriesCtrl.js
+++ public/javascripts/controllers/galleriesCtrl.js
@@ -1,4 +1,4 @@
-module.exports = function($scope) {
+module.exports = function($scope, $cookieStore) {
   $scope.data = {
     facebook: [ { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
                 { image:'img.png', thumb:'http://cdn.flaticon.com/png/256/7489.png'},
